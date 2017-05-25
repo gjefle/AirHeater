74422aeed37918e9dc0f257534543d5df31692f1
Gjermund Fleten <gjermufl@gmail.com>
2017-05-25 14:02:05 +0200

    Config values saved after debounce time instead of on input focus lost

--- AirHeater.AlarmMonitor/alarm-app/src/app/config/config-dialog.component.ts
+++ AirHeater.AlarmMonitor/alarm-app/src/app/config/config-dialog.component.ts
@@ -2,6 +2,7 @@
 import { TemperatureConfig } from '../models/temperatureConfig';
 import { MdDialog, MdDialogRef } from '@angular/material';
 import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'config-dialog',
@@ -16,7 +17,15 @@ export class ConfigDialog {
 
   }
   ngOnInit() {
-
+    // Set a delay of 500ms before changes to config values are saved.
+    this.highlarmForm.valueChanges
+      .debounceTime(500)
+      .distinctUntilChanged()
+      .subscribe(data => this.configChange());
+    this.lowAlarmForm.valueChanges
+      .debounceTime(500)
+      .distinctUntilChanged()
+      .subscribe(data => this.configChange());
   }
 
   configChange() {
